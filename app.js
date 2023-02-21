a71c3a6c83f6a8aa6ff0e7a2fbab6871fc0e88ba
MansuriArsh <mansuriarsh44@gmail.com>
2023-02-21 21:24:24 +0530

    changesss

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 require('dotenv').config({"path": "./config/config.env"});
+const http = require('http')
 const express = require('express');
 const app = express();
 const cookiParser = require('cookie-parser');
 const cloudinary = require("cloudinary")
 const cors = require("cors")
 const PORT = process.env.PORT || 6020
+
 
 
 
@@ -33,7 +35,13 @@ cloudinary.config({
     api_secret: process.env.API_SECRET
 })
 
+const server = http.createServer((req, res)=>{
+    res.statusCode = 200,
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Zeet Node')
+})
 
 app.listen(process.env.PORT, ()=>{
     console.log(`App listen on port ${PORT}`);
 })
+
